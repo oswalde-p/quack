ce263ba7c31f666b8068ac9e31275792465bb12d
oswalde-p <31041273+oswalde-p@users.noreply.github.com>
2019-07-07 21:30:40 +0200

    Add eslint, update sdk (#1)
    * Add settings definitions
    
    * Start listening to messages on device
    
    * Add eslint
    
    * Update fitbit-sdk to 3.1.2

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': false,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': [
+            'warn'
+        ]
+    }
+}
--- app/index.js
+++ app/index.js
@@ -1,53 +1,74 @@
-import clock from "clock";
-import document from "document";
-import { preferences } from "user-settings";
-import { battery } from "power";
-import { formatDate, getTimeStr, round } from "../common/utils";
-import { me as device } from "device";
-import { type } from "os";
-import { vibration } from "haptics"
+import clock from 'clock'
+import document from 'document'
+import { peerSocket } from 'messaging'
+import { battery } from 'power'
+import { formatDate, getTimeStr, round } from '../common/utils'
+import { me as device } from 'device'
+import { vibration } from 'haptics'
 
 // settings
-const CONFIG = {
-  NO_SYNC_LIMIT: 35,
-  SECOND_TIME_OFFSET: 11
+const settings = {
+  warningThreshold: 35,
+  secondTimeOffset: -8,
+  color: '#783c94',
+  showWarning: true,
+  showBattery: true,
+  showSecondTime: true
 }
 // Update the clock every minute
-clock.granularity = "minutes";
-
+clock.granularity = 'minutes'
 
 // Get a handle on the <text> element
-const timeText = document.getElementById("time");
-const message = document.getElementById("message");
-const dateText = document.getElementById("date");
-const batteryStatusText = document.getElementById("stat1");
-const secondTimeText = document.getElementById("stat2");
+const timeText = document.getElementById('time')
+const message = document.getElementById('message')
+const dateText = document.getElementById('date')
+const batteryStatusText = document.getElementById('stat1')
+const secondTimeText = document.getElementById('stat2')
 
-let messageOn = false;
-message.style.display = "none";
+message.style.display = 'none'
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-  let now = evt.date;
-  updateClock(now);
-  updateDate(now);
-  updateSecondTime(now, CONFIG.SECOND_TIME_OFFSET)
-  updateBattery();
+  let now = evt.date
+  updateClock(now)
+  updateDate(now)
+  updateSecondTime(now, settings.secondTimeOffset) // not sure why "name" is needed here but okay
+  updateBattery()
   updateConnectionStatus(now)
 }
 
+peerSocket.onmessage = function (evt){
+  // settings.warningThreshold = evt.data.warningThreshold
+  if ( evt.data.secondTimeOffset) settings.secondTimeOffset = evt.data.secondTimeOffset.name
+  settings.color = evt.data.color
+  settings.showWarning = evt.data.showWarning
+  settings.showBattery = evt.data.showBattery
+  settings.showSecondTime = evt.data.showSecondTime
+  console.log(settings.secondTimeOffset)
+  applySettings()
+}
+
+function applySettings() {
+  console.log('applying settings')
+  console.log(settings)
+  if (settings.showBattery) {
+    batteryStatusText.text = 'on'
+  } else {
+    batteryStatusText.text = 'off'
+  }
+}
 
 function updateConnectionStatus(now){
   let minutesSinceSync = (now - device.lastSyncTime) / (60*1000)
-  if (minutesSinceSync > CONFIG.NO_SYNC_LIMIT){
+  if (minutesSinceSync > settings.warningThreshold){
     showSyncWarning(minutesSinceSync)
     if (message.style.display === 'none'){
-        // showing warning for first time
-        warningVibrate()
+      // showing warning for first time
+      warningVibrate()
     }
     message.style.display = 'inline'
   }else{
-      message.style.display = 'none'
+    message.style.display = 'none'
   }
 
 }
@@ -64,7 +85,7 @@ function showSyncWarning(minutes){
 }
 
 function updateDate(now){
-  dateText.text = formatDate(now.getDate(), now.getMonth());
+  dateText.text = formatDate(now.getDate(), now.getMonth())
 }
 
 function updateSecondTime(now, offset){
@@ -72,13 +93,13 @@ function updateSecondTime(now, offset){
 }
 
 function updateBattery(){
-  batteryStatusText.text = Math.floor(battery.chargeLevel) + "%"
+  batteryStatusText.text = Math.floor(battery.chargeLevel) + '%'
 }
 
 function updateClock(now){
-    timeText.text = getTimeStr(now);
+  timeText.text = getTimeStr(now)
 }
 
 function warningVibrate(){
-    vibration.start("nudge-max")
+  vibration.start('nudge-max')
 }
