cd59f802b6c5ad13e3c24e13c30d3759142bbd30
oswalde-p <31041273+oswalde-p@users.noreply.github.com>
2019-07-08 01:22:17 +0200

    Fix warning bug (#3)
    * Update warning message on settings change
    
    * Don't show battery % when os icon is displayed
    
    * Change the granularity back to minutes

--- common/constants.js
+++ common/constants.js
@@ -2,10 +2,13 @@ const SETTINGS_EVENTS = {
   SHOW_BATTERY_STATUS: 'showBatteryStatus',
   SHOW_SECOND_TIME: 'showSecondTime',
   SECOND_TIME_OFFSET: 'secondTimeOffset',
-  SHOW_SYNC_WARNAING: 'showWarning',
+  SHOW_SYNC_WARNING: 'showWarning',
   SYNC_WARNING_THRESHOLD: 'warningThreshold',
   PRIMARY_COLOR: 'primaryColor',
   PRIMARY_COLOR_CUSTOM: 'primaryColorCustom'
 }
 
-export { SETTINGS_EVENTS }
+const DEFAULT_WARNING_THRESHOLD = 40
+const LOW_BATTERY_LIMIT = 16 // below 17%, OS low battery icon is shown
+
+export { SETTINGS_EVENTS, DEFAULT_WARNING_THRESHOLD, LOW_BATTERY_LIMIT }
